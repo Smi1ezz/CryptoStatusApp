be683362834899e7070cca7a3899c33e65e5b521
Smi1ezz <ilya.chainikov@mail.ru>
2022-11-04 14:37:17 +0300

    initial commit

---
+++ CryptoStatusApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CryptoStatusApp
+//
+//  Created by admin on 04.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
